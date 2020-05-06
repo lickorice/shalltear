e43559aadfb2f3e8e9bb2bac322d35c2fb4a2213
Carlos Panganiban <cgpanganiban@up.edu.ph>
2020-05-06 23:01:51 +0800

    Add reloadcogs command, Admin cog

---
+++ cogs/admin.py
@@ -0,0 +1,48 @@
+import logging
+
+import discord
+from discord.ext import commands
+
+from config import *
+from messages.core import *
+
+
+class Admin(commands.Cog):
+    def  __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['rlc',])
+    async def reloadcogs(self, ctx, *cogs):
+        """Reload specified cogs. If no cog is specified, reloads all cogs."""
+        if not len(cogs):
+            to_reload_cogs = ACTIVE_COGS
+        else:
+            to_reload_cogs = [cog for cog in cogs if cog in ACTIVE_COGS]
+
+        print("=====================================")
+        logging.info("Starting reloading sequence...")
+        print("=====================================")
+        successful_cogs = []
+        for cog in to_reload_cogs:
+            logging.info("Reloading cog '{}'".format(cog))
+            try:
+                self.bot.reload_extension(cog)
+                successful_cogs.append(cog)
+                logging.info("Successfully reloaded cog '{}'".format(cog))
+            except Exception as e:
+                logging.error(e)
+                logging.error("Reload failed for cog '{}'".format(cog))
+
+        print("=====================================")
+        cogs_success, total_cogs = len(successful_cogs), len(to_reload_cogs)
+        logging.info("Successfully reloaded {}/{} cogs! ".format(cogs_success, total_cogs))
+        print("=====================================")
+        if cogs_success:
+            await ctx.send("Successfully reloaded:\n{}".format('\n'.join(successful_cogs)))
+        else:
+            await ctx.send("No cogs have been successfully reloaded.")
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
+
