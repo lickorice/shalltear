4df84b5ada707791e2b2e15c0a6918745a0e3538
Carlos Panganiban <cgpanganiban@up.edu.ph>
2020-05-06 23:14:48 +0800

    Add command error handling and reactivate on_message

--- bot.py
+++ bot.py
@@ -7,6 +7,10 @@ class BotCore(commands.Bot):
     async def on_ready(self):
         logging.info("Logged on as [{}]".format(self.user))
 
-    # async def on_message(self, message):
-    #     logging.info("Message from {0.author}: {0.content}".format(message))
+    async def on_message(self, message):
+        logging.debug("Message from {0.author}: {0.content}".format(message))
 
+        await self.process_commands(message)
+
+    async def on_command_error(self, ctx, err):
+        logging.error("Command error: {0} [author: {1.author}][cmd: {1.command}]".format(err, ctx))
