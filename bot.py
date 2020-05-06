c5f09f1f57cebe56303dcf389058cd423d7f2667
Carlos Panganiban <cgpanganiban@up.edu.ph>
2020-05-06 22:05:20 +0800

    Comment on_message to allow for command processing

--- bot.py
+++ bot.py
@@ -7,6 +7,6 @@ class BotCore(commands.Bot):
     async def on_ready(self):
         logging.info("Logged on as [{}]".format(self.user))
 
-    async def on_message(self, message):
-        logging.info("Message from {0.author}: {0.content}".format(message))
+    # async def on_message(self, message):
+    #     logging.info("Message from {0.author}: {0.content}".format(message))
 
