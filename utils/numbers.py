00a135a1248fb6f9f68941d57d319502f84cb9d1
Carlos Panganiban <infocarl45@gmail.com>
2020-05-10 05:53:01 +0800

    Add human readable large numbers.

---
+++ utils/numbers.py
@@ -0,0 +1,10 @@
+import math
+
+millnames = ['',' K',' M',' B',' T', ' Qd', 'Qt', ' Sx', ' Sp', ' Oc', ' No']
+
+def millify(n):
+    n = float(n)
+    millidx = max(0,min(len(millnames)-1,
+                        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+
+    return '{:.2f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
